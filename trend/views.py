a74d3258c8e2eaa7e5d1eb563db2d0da4f6b62dd
ndukwesamuel <ndukwesamuel23@yahoo.com>
2021-03-18 22:51:28 -0700

    first commit

---
+++ trend/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib import  messages
+ 
+from django.http import HttpResponse
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.decorators import login_required
+
+from trend.models import *
+from trend.forms import *
+
+from django.shortcuts import render
+from django.contrib.auth.models import Group
+
+from .models import *
+
+
+
+def home(request):
+    form= contactForm(request.POST or None, request.FILES or None)
+    you = contactModel.objects.all()
+    if form.is_valid():
+        form.save()
+        form = contactForm
+
+
+    context ={'form':form}
+    return render(request, 'index.html' , context)
+
+
+# # @login_required(login_url='clicked')
+# def Hire_developers(request):
+#     form= CompanyForm(request.POST or None, request.FILES or None)
+#     if form.is_valid():
+#         form.save()
+
+#     context = {'form':form}
+#     return render(request, 'Hire_developers.html', context)
+
